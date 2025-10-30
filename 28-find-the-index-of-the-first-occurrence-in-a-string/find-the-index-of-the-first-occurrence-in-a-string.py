6f2ec981be84843bcab58ffa897b9122ee7ed22a
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2025-10-30 13:55:51 +0300

    Time: 54 ms (2.78%) | Memory: 12.4 MB (81.22%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -5,30 +5,30 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        initial = needle[0]
+
         found = False
         found_index = -1
         haystack_index = 0
-        while haystack_index < len(haystack) and found == False:
-            needle_index = 0
-            
-            while needle_index < len(needle) and (haystack_index + needle_index) < len(haystack):
-                if(needle[needle_index] != haystack[haystack_index + needle_index]):
-                    needle_index = len(needle) + 1
-                
-                else:
-                    needle_index += 1
-                
-            if needle_index == len(needle):
-                found = True
-                found_index = haystack_index
-                haystack_index = len(haystack)
+        needle_index = 0
+        haystack_index_renewal = 0
+        while haystack_index + needle_index < len(haystack) and found == False:
+            if needle[needle_index] == haystack[haystack_index + needle_index]:
+                if needle[0] == haystack[haystack_index + needle_index] and needle_index > 0 and haystack_index_renewal == 0:
+                    haystack_index_renewal = haystack_index + needle_index
+                needle_index += 1
+                if needle_index == len(needle):
+                    found_index = haystack_index
+                    found = True
+                    
             else:
-                haystack_index += 1
-                while haystack_index < len(haystack) and haystack[haystack_index] != initial:
+                if haystack_index_renewal != 0:
+                    haystack_index = haystack_index_renewal
+                    haystack_index_renewal = 0
+                elif needle_index != 0:
+                    haystack_index += needle_index
+                else:
                     haystack_index += 1
-            print(haystack_index)
+                needle_index = 0
+            print(haystack_index, needle_index)
         return found_index
-
-
             
