f2c86be6a5bcbd0c432a9c8fdbbdc7fc6d376a49
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2025-11-02 18:23:02 +0300

    Time: 251 ms (17.62%) | Memory: 12.6 MB (10.79%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        low, high = 0, x
+        while(high >= low):
+            center = int((high + low) / 2)
+            print(low, high, center)
+            if center * center == x:
+                return center
+            elif low * low == x:
+                return low
+            elif high * high == x:
+                return high
+            elif center * center < x:
+                low = center
+            elif center * center > x:
+                high = center
+            if high - low == 1:
+                return low
+
+    
