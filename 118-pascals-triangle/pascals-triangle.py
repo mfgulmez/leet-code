1642f02c4fb3a26444ecc5318c88bb222a346ee6
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2025-11-06 04:05:13 +0300

    Time: 11 ms (3.77%) | Memory: 12.3 MB (99.52%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        for i in range(0, numRows):
+            row = []
+            if i > 1:
+                row += [1]
+                previous_row = result[i - 1]
+                print(previous_row)
+                j = 0
+                while j < i - 1:
+                    row += [previous_row[j] + previous_row[j + 1]]
+                    j += 1
+
+                row += [1]
+            elif i == 1:
+                row = [1, 1]
+            else:
+                row = [1]
+            result.append(row)
+        return result
+            
+        
