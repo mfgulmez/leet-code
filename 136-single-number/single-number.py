3a914118dc3943994069abc1ed956384aa604120
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2025-11-07 22:26:01 +0300

    Time: 4 ms (58.77%) | Memory: 14 MB (72.09%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -4,4 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return reduce(operator.xor, nums)
+        result = nums[0]
+        for i in range(1, len(nums)):
+            result = result ^ nums[i]
+        return result
