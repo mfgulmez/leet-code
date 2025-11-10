80630edc9aba2a101c020ec8d4164846cfb49a10
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2025-11-10 23:54:32 +0300

    Time: 5 ms (60.24%) | Memory: 17.1 MB (94.11%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -5,11 +5,11 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    def depth(self, treeNode):
+    def depth_comparison(self, treeNode):
         if treeNode == None:
             return -1, True
-        depth_left, left_balanced = self.depth(treeNode.left)
-        depth_right, right_balanced = self.depth(treeNode.right)
+        depth_left, left_balanced = self.depth_comparison(treeNode.left)
+        depth_right, right_balanced = self.depth_comparison(treeNode.right)
         
         return (max(depth_left, depth_right) + 1, abs(depth_left - depth_right) <= 1 and left_balanced and right_balanced)
         
@@ -21,4 +21,4 @@ class Solution(object):
         
 
             
-        return self.depth(root)[1]
+        return self.depth_comparison(root)[1]
