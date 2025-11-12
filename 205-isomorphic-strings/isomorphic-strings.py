f8c6cec65261673c82bec8ef4e2a19acc2ce8a18
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2025-11-12 17:23:25 +0300

    Time: 8 ms (71.51%) | Memory: 12.6 MB (98.42%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        index = 0
+        t_mapping = {}
+        s_mapping = {}
+        while index < len(s):
+            t_char, s_char = s[index], t[index]
+            if t_char in t_mapping:
+                if s_char != t_mapping[t_char]:
+                    return False
+            if s_char in s_mapping:
+                if t_char != s_mapping[s_char]:
+                    return False          
+
+            t_mapping[t_char] = s_char
+            s_mapping[s_char] = t_char
+            index += 1
+        return True
