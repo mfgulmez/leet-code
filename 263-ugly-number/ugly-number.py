4336bd06e4870ccdc13a2034c69c0a7d39af3251
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2025-11-12 17:59:12 +0300

    Time: 0 ms (100.00%) | Memory: 12.4 MB (49.91%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -6,14 +6,14 @@ class Solution(object):
         """
         if n == 0:
             return False
-        if n % 2 == 0:
-            while n % 2 == 0:
+  
+        while n % 2 == 0 or n % 3 == 0 or n % 5 == 0:
+            if n % 2 == 0:
                 n /= 2
-        if n % 3 == 0:
-            while n % 3 == 0:
+            if n % 3 == 0:
                 n /= 3
-        if n % 5 == 0:
-            while n % 5 == 0:
+            if n % 5 == 0:
                 n /= 5
+    
         return n == 1
         
