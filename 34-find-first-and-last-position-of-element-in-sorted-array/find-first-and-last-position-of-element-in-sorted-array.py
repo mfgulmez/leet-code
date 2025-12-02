693afefed50278adb70856762a4d6c78d97a0fb7
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2025-12-02 18:29:25 +0300

    Time: 3 ms (16.53%) | Memory: 13.2 MB (29.88%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -6,18 +6,17 @@ class Solution(object):
         :rtype: List[int]
         """
         length = len(nums)
+        if length == 0:
+            return [-1, -1]
         low, high = 0, length - 1
-
-        while low < length and high > -1:
-            
-            if (nums[low] == target and nums[high] <= target 
-                or nums[high] == target and nums[low] >= target):
-                return [low, high]
-            elif low > high:
-                return [-1, -1]
+    
+        while low < length and high > -1 and (nums[low] < target or nums[high] > target):
             if nums[low] < target:
                 low += 1
             if nums[high] > target:
                 high -= 1
-        
+        if low >= length or high < 0:
+            return [-1, -1]
+        if nums[low] == target or nums[high] == target:
+            return [low, high]
         return [-1, -1]
