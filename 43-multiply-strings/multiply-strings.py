4c4bc8327126c9a95f67df5d2a084c22d1741ad2
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2025-12-10 20:35:17 +0300

    Time: 155 ms (19.71%) | Memory: 12.6 MB (12.00%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -4,21 +4,16 @@ class Solution(object):
             return "0"
 
         n1, n2 = len(num1), len(num2)
-
         res = [0] * (n1 + n2)
 
         for i in range(n1 - 1, -1, -1):
             for j in range(n2 - 1, -1, -1):
-                p = int(num1[i]) * int(num2[j])
-                pos_low = i + j + 1
-                pos_high = i + j
+                mul = int(num1[i]) * int(num2[j])
+                s = mul + res[i + j + 1]
+                res[i + j + 1] = s % 10
+                res[i + j] += s // 10
+                
+        if res[0] == 0:
+            res = res[1:]
 
-                total = res[pos_low] + p
-                res[pos_low] = total % 10         
-                res[pos_high] += total // 10       
-
-        k = 0
-        while k < len(res) and res[k] == 0:
-            k += 1
-
-        return ''.join(map(str, res[k:])) if k < len(res) else "0"
+        return ''.join(map(str, res))
