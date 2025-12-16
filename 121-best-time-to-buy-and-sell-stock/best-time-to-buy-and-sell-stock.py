2913f23f7e645a3606d9cccc01a0278bab890d5e
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2025-12-16 15:43:22 +0300

    Time: 479 ms (5.10%) | Memory: 19.1 MB (71.55%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,21 +1,26 @@
 class Solution(object):
     def getMaxProfit(self, low, high, prices):
-        if low + 1 < high:
-            mid = (low + high) // 2
-            min_lower, max_upper = min(prices[low:mid]), max(prices[mid:high + 1])
-            intersect_profit  = max_upper - min_lower
+        if low == high:
+            return prices[low], prices[low], 0
 
-            return max(intersect_profit, self.getMaxProfit(mid, high, prices), self.getMaxProfit(low, mid, prices))
-        else:
-            if low < high:
-                return prices[high] - prices[low]
+        mid = (low + high) // 2
+
+        left_min, left_max, left_profit = self.getMaxProfit(low, mid, prices)
+        right_min, right_max, right_profit = self.getMaxProfit(mid + 1, high, prices)
+
+        cross_profit = right_max - left_min
+
+        min_price = min(left_min, right_min)
+        max_price = max(left_max, right_max)
+        max_profit = max(left_profit, right_profit, cross_profit)
+
+        return min_price, max_price, max_profit
+
     def maxProfit(self, prices):
         """
         :type prices: List[int]
         :rtype: int
         """
-        low, high = 0, len(prices) - 1
-        maxProfit = self.getMaxProfit(low, high, prices)
-        if maxProfit < 0:
+        if not prices:
             return 0
-        return maxProfit
+        return self.getMaxProfit(0, len(prices) - 1, prices)[2]
