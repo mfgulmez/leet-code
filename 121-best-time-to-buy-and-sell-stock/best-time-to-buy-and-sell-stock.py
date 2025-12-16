a48851d7d3bafe2dca91f68d4b51c52193a1da7b
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2025-12-16 15:35:33 +0300

    Time: 725 ms (5.10%) | Memory: 19 MB (90.06%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -2,12 +2,10 @@ class Solution(object):
     def getMaxProfit(self, low, high, prices):
         if low + 1 < high:
             mid = (low + high) // 2
-            lower_profit = prices[mid] - prices[low]
-            upper_profit = prices[high] - prices[mid]
             min_lower, max_upper = min(prices[low:mid]), max(prices[mid:high + 1])
             intersect_profit  = max_upper - min_lower
 
-            return max(intersect_profit, lower_profit, upper_profit, self.getMaxProfit(mid, high, prices), self.getMaxProfit(low, mid, prices))
+            return max(intersect_profit, self.getMaxProfit(mid, high, prices), self.getMaxProfit(low, mid, prices))
         else:
             if low < high:
                 return prices[high] - prices[low]
