0080a56a654fe13cb1aa2ad6789213d1ee7523f8
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2025-12-16 17:32:43 +0300

    Time: 8 ms (46.04%) | Memory: 13.7 MB (10.95%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,13 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        index, count = 0, 0
-        length = len(nums)
-        initial_operator = 0
-        while count < length:
-            if nums[index] == 0:
-                nums.pop(index)
-                nums.insert(len(nums), 0)
-            else:
-                index += 1
-            count += 1
+        write = 0
+        for read in range(len(nums)):
+            if nums[read] != 0:
+                nums[write] = nums[read]
+                write += 1
+        for i in range(write, len(nums)):
+            nums[i] = 0
