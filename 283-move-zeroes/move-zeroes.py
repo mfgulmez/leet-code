27ca84a02502284c3d2b21e9a669f346a0e76e58
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2025-12-16 17:28:54 +0300

    Time: 507 ms (10.60%) | Memory: 13.5 MB (82.83%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        index, count = 0, 0
+        length = len(nums)
+        initial_operator = 0
+        while count < length:
+            print(index, nums[index])
+            if nums[index] == 0:
+                nums.pop(index)
+                nums.insert(len(nums), 0)
+            else:
+                index += 1
+            count += 1
