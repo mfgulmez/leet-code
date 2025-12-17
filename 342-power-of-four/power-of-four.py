e0ab1d09d83f3c144270f1a0366f3fc5d69934f8
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2025-12-17 19:57:09 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (11.54%) - LeetSync

--- 342-power-of-four/power-of-four.py
+++ 342-power-of-four/power-of-four.py
@@ -4,7 +4,6 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        print(n & (n - 1))
         if n & (n - 1) == False and (n - 1) % 3 == 0:
             return True
         return False
