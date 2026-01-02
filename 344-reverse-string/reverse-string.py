aa9faffd062b2ef60818817dff0b4e8cd6a6f163
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2026-01-03 00:34:09 +0300

    Time: 3 ms (54.78%) | Memory: 20.7 MB (6.50%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -5,6 +5,5 @@ class Solution(object):
         :rtype: None Do not return anything, modify s in-place instead.
         """
         index = 0
-        while index < (len(s) // 2):
+        for index in range(len(s) // 2):
             s[index] , s[len(s) - index - 1] = s[len(s) - index - 1], s[index]
-            index += 1
