8237af127cb92e2e6d5da49d9945f09a703b0e0d
Mehmet Fatih Gülmez <80184738+mfgulmez@users.noreply.github.com>
2026-01-07 23:18:59 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (46.24%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -4,13 +4,5 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        s = s[::-1]
-        begin = False
-        i = 0
-        while i < len(s) and s[i] == " ":
-            i += 1
-        first_letter = i
-        while i < len(s) and s[i] != " ":
-            i += 1
-        return i - first_letter
-        
+        words = s.split()
+        return len(words[len(words) - 1])
